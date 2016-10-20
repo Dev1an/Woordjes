596ae0e19e7135322b44ceb3eff43e84da36a540
Damiaan Dufaux <damiaan@dpro.be>
2016-10-20 03:03:13 +0200

    Modify toolbar

---
+++ Woordjes Desktop/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  Woordjes
+//
+//  Created by Damiaan on 20-10-16.
+//  Copyright © 2016 Damiaan Dufaux. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        window?.titleVisibility = .hidden
+    }
+    
+}
